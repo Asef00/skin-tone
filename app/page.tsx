ef700dc5bb42504a231297ab03bfa842acf9acce
Asef <asefhosseini00@gmail.com>
2025-09-27 18:41:14 +0330

    refactor: adjust layout for better responsiveness and spacing in main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import ResultsCard from '@/app/components/results-card'
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="grid grid-cols-2 gap-16 max-w-7xl mx-auto">
-        <div className="flex flex-col gap-[32px]">
+    <div className="min-h-screen p-8 pb-20 sm:p-20">
+      <main className="grid md:grid-cols-2 gap-8 max-w-7xl mx-auto">
+        <div className="flex flex-col gap-8">
           <ImageUploadCard />
           <ColorPickerCard />
         </div>
