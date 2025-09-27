1ddaa08a39c3283f66173f38d5496b46a4dd26d8
Asef <asefhosseini00@gmail.com>
2025-09-27 18:35:33 +0330

    refactor: update main layout structure for improved responsiveness and organization

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import ResultsCard from '@/app/components/results-card'
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2">
-        <ImageUploadCard />
-        <ColorPickerCard />
+    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="grid grid-cols-2 gap-16 max-w-7xl mx-auto">
+        <div className="flex flex-col gap-[32px]">
+          <ImageUploadCard />
+          <ColorPickerCard />
+        </div>
         <ResultsCard />
       </main>
     </div>
